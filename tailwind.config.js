f53436da5170a93974991ee083f0fb6fe219fb45
ruchit3601 <ruchitchudasama123@gmail.com>
2021-07-11 10:50:17 +0530

    working on HOC

---
+++ tailwind.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors:{
+        ruchit:{
+          
+
+ 
+
+  50: '#ddfcff',
+  100: '#b8eef5',
+  200: '#91e2ed',
+  300: '#67d6e4',
+  400: '#41cadb',
+  500: '#2ab0c2',
+  600: '#1b8998',
+  700: '#0c626d',
+  800: '#003c43',
+  900: '#00161a',
+
+
+        }
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
