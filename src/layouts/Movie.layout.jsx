2a12d2a5c6cb596bf22a4bdf1dcedec7070dbbfe
ruchit3601 <ruchitchudasama123@gmail.com>
2021-07-25 20:34:49 +0530

    payment gateway integration

--- src/layouts/Movie.layout.jsx
+++ src/layouts/Movie.layout.jsx
@@ -11,7 +11,7 @@ import { MovieContext } from '../context/movie.context';
 const MovieLayout = (props) => {
 
     const { id } = useParams();
-    const { movie, setMovie } = useContext(MovieContext);
+    const { setMovie } = useContext(MovieContext);
 
     useEffect(() =>{
         const requestMovie = async () => {
@@ -19,7 +19,7 @@ const MovieLayout = (props) => {
             setMovie(getMovieData.data);
         };
         requestMovie();
-    }, []);
+    }, [id]);
 
     return (
         <>
--- src/pages/Movie.page.jsx
+++ src/pages/Movie.page.jsx
@@ -1,18 +1,58 @@
-import React from 'react';
 
+import React,{ useContext, useState,useEffect } from 'react';
 import { FaCcVisa, FaCcApplePay } from "react-icons/fa";
+import axios from 'axios';
+import { useParams } from 'react-router';
+import Slider from 'react-slick';
+
+
+
 
 //component
 import MovieHero from '../components/MovieHero/MovieHero.component';
 import Cast from '../components/Cast/Cast.component';
 import PosterSlider from "../components/PosterSlider/PosterSlider.component";
 
-//config
-import TempPosters from "../config/TempPoster.config"
 
+
+//context
+import { movie ,MovieContext } from '../context/movie.context';
 
 
 const Movie = () => {
+
+  const { id } = useParams();
+  const{ movie } = useContext(MovieContext);
+  const [cast, setCast] = useState( [] );
+  const [similarMovies, setSimilarMovies] = useState( [] );
+  const [recommended, setRecommended] = useState( [] );
+
+
+
+  useEffect(() => {
+  const requestCast = async () => {
+      const getCast = await axios.get(`/movie/${id}/credits`);
+      setCast(getCast.data.cast);
+    };
+    requestCast();
+  }, [id]);
+
+  useEffect(() => {
+    const requestSimilarMovies = async () => {
+        const getSimilarMovies = await axios.get(`/movie/${id}/similar`);
+        setSimilarMovies(getSimilarMovies.data.results);
+    };
+    requestSimilarMovies();
+}, [id]);
+
+useEffect(() => {
+  const requestRecommendedMovies = async () => {
+      const getRecommendedMovies = await axios.get(`/movie/${id}/recommendations`);
+      setRecommended(getRecommendedMovies.data.results);
+  };
+  requestRecommendedMovies();
+}, [id]);
+   
   const settings = {
     infinite: false,
     speed: 500,
@@ -45,6 +85,38 @@ const Movie = () => {
       },
     ],
   };
+  const settingsCast = {
+    infinite: false,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 3,
+          infinite: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 5,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   
 
     return (
@@ -54,8 +126,7 @@ const Movie = () => {
             <div className="flex flex-col items-start gap-3">
             <h2 className="text-gray-800 font-bold text-2xl">About the movie</h2>
             <p>
-            Bruce Wayne and Diana Prince try to bring the metahumans of Earth together after the death of Clark Kent. 
-            Meanwhile, Darkseid sends Steppenwolf to Earth with an army to subjugate humans.
+            { movie.overview }
             </p>
             </div>
             <div className="my-8">
@@ -97,27 +168,24 @@ const Movie = () => {
         </div>
         
         <div className="my-8">
-                <hr />
+            <hr />
         </div>
 
-        <div className="my-9">
+        <div className="my-8">
         <h2 className="text-gray-800 font-bold text-2xl mb-4">Cast & crew</h2>
 
-        <div className="flex flex-wrap gap-3">
+       
+          <Slider{ ...settingsCast }>
+          {cast.map((castdata) => (
           <Cast 
-            image="https://in.bmscdn.com/iedb/artist/images/website/poster/large/ben-affleck-292-12-09-2017-05-12-16.jpg"
-            castName="Ben Affleck"
-            role="Batman"  />
-            <Cast 
-            image="https://in.bmscdn.com/iedb/artist/images/website/poster/large/henry-cavill-23964-04-05-2020-04-25-14.jpg"
-            castName="Henry Cavil"
-            role="Superman"  />
-            <Cast 
-            image="https://in.bmscdn.com/iedb/artist/images/website/poster/large/gal-gadot-11088-17-10-2017-11-45-36.jpg"
-            castName="Gal Gadol"
-            role="SWonder women"  />
+          image={`https://image.tmdb.org/t/p/original/${castdata.profile_path}`}
+          castName={ castdata.original_name }
+          role={ castdata.character }  
+          />
+          ))}
+          </Slider>
   
-          </div>
+          
         </div>
         <div className="my-8">
           <hr />
@@ -125,7 +193,7 @@ const Movie = () => {
       <div className="my-8">
           <PosterSlider 
           config={settings}
-             images={TempPosters} 
+             images={similarMovies} 
              title="You might also like" 
              isDark={false} />
       </div>
@@ -135,7 +203,7 @@ const Movie = () => {
       <div className="my-8">
           <PosterSlider 
           config={settings}
-             images={TempPosters} 
+             images={recommended} 
              title="BMS XCLUSIV" 
              isDark={false} />
       </div>
